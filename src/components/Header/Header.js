ee60d14454713cb371297268110c3ce27ddfa1b4
alamgirjuab <alamgirjuab@gmail.com>
2021-11-25 00:18:43 +0600

    header changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='top-header'>
-            <h1>Jahangirnagar University Alumni Association Re-Union-2021</h1>
+            <h1>Jahangirnagar University Alumny Association Re-Union-2021</h1>
             <h3>Members Registration and contribution collection</h3>
             <h1>Total Budget : 1.0 Million</h1>
         </div>
